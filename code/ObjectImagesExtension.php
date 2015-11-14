acb73e4b299632270cf6df7aef4f2c31e8bcbbed
rasstislav <brandobur@webmax.sk>
2015-11-15 00:56:39 +0100

    Update ObjectImagesExtension.php

--- code/ObjectImagesExtension.php
+++ code/ObjectImagesExtension.php
@@ -15,24 +15,26 @@ class ObjectImagesExtension extends DataExtension {
 	
 	public function updateCMSFields(FieldList $fields) {      
          // Use SortableUploadField instead of UploadField!
-        $limit = 20;
-        $uploadClass = (class_exists("SortableUploadField") && $this->owner->Sorter == "SortOrder") ? "SortableUploadField" : "UploadField";
-        $imageField = $uploadClass::create('Images', _t("Object.IMAGESUPLOADLABEL", "Images"));
-        $imageField->setDescription(_t("Object.IMAGESUPLOADLIMIT","Images count limit: " . $limit));
-		$imageField->setConfig('allowedMaxFileNumber', $limit);
-		$imageField->setFolderName('Uploads/'.$this->owner->ClassName.'/'.$this->owner->ID);
-		
-		$dropdownSorter = DropdownField::create('Sorter', _t("Object.IMAGESSORTER", "Sort imags by: "))->setSource($this->owner->dbObject('Sorter')->enumValues());
-		
-		if ($this->owner->Sorter == "SortOrder")  {
-			$message =(class_exists("SortableUploadField")) ? _t("Object.IMAGESUPLOADHEADING", "<span style='color: green'>Sort images by draging thumbnail</span>") : _t("Object.IMAGESUPLOADHEADINGWRONG", "<span style='color: red'>Sorting images by draging thumbnails (SortOrder) not allowed. Missing module SortabeUploadField.</span>"); 
-		} else {
-			$message =  _t("Object.IMAGESSORTERNOTICE", "Correct image sorting is visible on frontend only (if Sort by = Title, ID, Name)");
-		}
-		
-		$fields->addFieldToTab('Root.Images', HeaderField::create('ImagesNotice', $message)->setHeadingLevel(4));
-		$fields->addFieldToTab('Root.Images', $dropdownSorter);
-        $fields->addFieldToTab('Root.Images', $imageField);
+	        $limit = 20;
+	        $uploadClass = (class_exists("SortableUploadField") && $this->owner->Sorter == "SortOrder") ? "SortableUploadField" : "UploadField";
+	        $imageField = $uploadClass::create('Images', _t("Object.IMAGESUPLOADLABEL", "Images"));
+	        $imageField->setDescription(sprintf(_t("Object.IMAGESUPLOADLIMIT","Images count limit: %s"), $limit));
+			$imageField->setConfig('allowedMaxFileNumber', $limit);
+			$imageField->setFolderName('Uploads/'.$this->owner->ClassName.'/'.$this->owner->ID);
+			
+			$dropdownSorter = DropdownField::create('Sorter', _t("Object.IMAGESSORTER", "Sort imags by: "))->setSource($this->owner->dbObject('Sorter')->enumValues());
+			
+			if ($this->owner->Sorter == "SortOrder")  {
+				$message =(class_exists("SortableUploadField")) ? _t("Object.IMAGESUPLOADHEADING", "<span style='color: green'>Sort images by draging thumbnail</span>") : _t("Object.IMAGESUPLOADHEADINGWRONG", "<span style='color: red'>Sorting images by draging thumbnails (SortOrder) not allowed. Missing module SortabeUploadField.</span>"); 
+			} else {
+				$message =  _t("Object.IMAGESSORTERNOTICE", "Correct image sorting is visible on frontend only (if Sort by = Title, ID)");
+			}
+			
+			$fields->addFieldToTab('Root.Images', HeaderField::create('ImagesNotice', $message)->setHeadingLevel(4));
+			$fields->addFieldToTab('Root.Images', $dropdownSorter);
+        	$fields->addFieldToTab('Root.Images', $imageField);
+        	
+        	$fields->fieldByName('Root.Images')->setTitle(_t("Object.IMAGESTAB", "Images"));
 	}
 	
 	public function SortedImages(){
@@ -42,7 +44,6 @@ class ObjectImagesExtension extends DataExtension {
 	public function MainImage() {
 		return $this->owner->Images()->Sort($this->owner->Sorter)->limit(1)->First();
 	}
-				
 }
 
 // EOF
