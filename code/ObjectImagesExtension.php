ef5c8c985dabd6969a0f5a60df11e5356ed952f0
pali ondras <ondras@webmax.sk>
2018-02-20 15:09:11 +0100

    ss4 initial version

--- code/ObjectImagesExtension.php
+++ code/ObjectImagesExtension.php
@@ -1,17 +1,27 @@
 <?php
 
-class ObjectImagesExtension extends DataExtension {
+use SilverStripe\Core\Extension;
+use SilverStripe\Assets\Image;
+use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\literalField;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+
+class ObjectImagesExtension extends Extension {
 
 	private static $allow_images = true;
-	private static $images_count = 20;
 
 	private static $db = array(
 		'Sorter' => 'Enum("SortOrder, Title, Name, ID")'
 	);
 
 	private static $many_many = array(
-		'Images' => 'Image'
+		'Images' => Image::class
 	);
+
+    private static $owns = [
+        'Images'
+    ];
 
 	private static $many_many_extraFields = array(
 		'Images' => array('SortOrder' => 'Int')
@@ -23,34 +33,27 @@ class ObjectImagesExtension extends DataExtension {
 
 		$owner = $this->owner;
 		if ($owner::config()->allow_images) {
-			$limit = $owner::config()->images_count;
 
-			$uploadClass = (class_exists("SortableUploadField") && $this->owner->Sorter == "SortOrder") ? "SortableUploadField" : "UploadField";
+			//$uploadClass = (class_exists("SortableUploadField") && $this->owner->Sorter == "SortOrder") ? "SortableUploadField" : "UploadField";
+			//$imageField = $uploadClass::create('Images');
+            $imageField = UploadField::create('Images');
 
-			$imageField = $uploadClass::create('Images');
-			$imageField->setConfig('allowedMaxFileNumber', $limit);
 			$imageField->setFolderName('Uploads/'.$this->owner->ClassName.'/'.$this->owner->ID);
+            $imageField->getValidator()->setAllowedExtensions(array('jpg', 'jpeg', 'png', 'gif'));
 
-			if ($limit==1) {
-				$imagesTab->setTitle(_t("Object.IMAGETAB", "Images"));
-				$imageField->setTitle(_t("Object.IMAGEUPLOADLABEL", "Image"));
-			}
-			else {
-				$imagesTab->setTitle(_t("Object.IMAGESTAB", "Images"));
-				$imageField->setTitle(_t("Object.IMAGESUPLOADLABEL", "Images"));
-				$imageField->setDescription(sprintf(_t("Object.IMAGESUPLOADLIMIT","Images count limit: %s"), $limit));
+            $imagesTab->setTitle(_t("Object.IMAGESTAB", "Images"));
+            $imageField->setTitle(_t("Object.IMAGESUPLOADLABEL", "Images"));
 
-				if ($this->owner->Sorter == "SortOrder")  {
-					$message = (class_exists("SortableUploadField")) ? _t("Object.IMAGESUPLOADHEADING", "<span style='color: green'>Sort images by draging thumbnail</span>") : _t("Object.IMAGESUPLOADHEADINGWRONG", "<span style='color: red'>Sorting images by draging thumbnails (SortOrder) not allowed. Missing module SortabeUploadField.</span>");
-				} else {
-					$message = _t("Object.IMAGESSORTERNOTICE", "Correct image sorting is visible on frontend only (if Sort by = Title, ID)");
-				}
+            if ($this->owner->Sorter == "SortOrder")  {
+                $message = (class_exists("SortableUploadField")) ? _t("Object.IMAGESUPLOADHEADING", "<span style='color: green'>Sort images by draging thumbnail</span>") : _t("Object.IMAGESUPLOADHEADINGWRONG", "<span style='color: red'>Sorting images by draging thumbnails (SortOrder) not allowed. Missing module SortabeUploadField.</span>");
+            } else {
+                $message = _t("Object.IMAGESSORTERNOTICE", "Correct image sorting is visible on frontend only (if Sort by = Title, ID)");
+            }
 
-				$dropdownSorter = DropdownField::create('Sorter', _t("Object.IMAGESSORTER", "Sort images by: "))->setSource($this->owner->dbObject('Sorter')->enumValues());
-				$fields->addFieldToTab('Root.Images', $dropdownSorter);
+            $dropdownSorter = DropdownField::create('Sorter', _t("Object.IMAGESSORTER", "Sort images by: "))->setSource($this->owner->dbObject('Sorter')->enumValues());
+            $fields->addFieldToTab('Root.Images', $dropdownSorter);
 
-				$fields->addFieldToTab('Root.Images', HeaderField::create('ImagesNotice', $message)->setHeadingLevel(4));
-			}
+            $fields->addFieldToTab('Root.Images', LiteralField::create('ImagesNotice', $message));
 
 			$fields->addFieldToTab('Root.Images', $imageField);
 		}
